d43df9e3c5ed6b796f074bab0fa1fed8b1145832
msudenur <msudenur.kus552@gmail.com>
2026-04-23 23:21:23 +0300

    Updated UserManager class

--- UserManager.java
+++ UserManager.java
@@ -1,7 +1,61 @@
 package shopix;
 
-import java.io.Serializable;
+	
+	import java.util.ArrayList;
 
-public class UserManager {
+	public class UserManager {
+
+	    private ArrayList<User> users;
+
+	
+	    public UserManager() {
+	        users = new ArrayList<>();
+	    }
+
+	    // Kullanıcı kayıt 
+	    public boolean register(User user) {
+
+	        if (usernameExists(user.getUsername())) {
+	            System.out.println("Bu kullanıcı adı zaten var!");
+	            return false;
+	        }
+
+	        users.add(user);
+	        System.out.println("Kayıt başarılı!");
+	        return true;
+	    }
+
+	    // Giriş yapma
+	    public User login(String username, String password) {
+
+	        for (User u : users) {
+	            if (u.getUsername().equals(username) &&
+	                u.getPassword().equals(password)) {
+
+	                return u; 
+	            }
+	        }
+
+	        return null; 
+	    }
+
+	    // Kullanıcı adı var mı kontrolü
+	    public boolean usernameExists(String username) {
+
+	        for (User u : users) {
+	            if (u.getUsername().equals(username)) {
+	                return true;
+	            }
+	        }
+
+	        return false;
+	    }
+
+	
+	    public ArrayList<User> getUsers() {
+	        return users;
+	    }
+	}
+	
 
 }
